5bb1a23dd7f3e190dfeaff891fcad34edeb25efc
rscav <rscav06@gmail.com>
2025-09-16 09:50:49 -0400

    Start PCA4: File Setup

---
+++ LCA/rc_LCA4.py
@@ -0,0 +1,18 @@
+# File: rc_LCA4.py
+# Description: 
+# Input(s): 
+# Output(s): 
+# By: Rodrigo Casanova-Aleman
+#
+# Three things I learned:
+# 1. 
+# 2. 
+# 3.
+
+# define main() function
+def main():
+    #
+
+# call main() function
+if __name__ == "__main__":
+    main()
